e03bbeb9127e72f3ae7cbaca66f9cba6a25ad1fc
Imcasero <diegocaserosmr@gmail.com>
2024-06-23 16:59:19 +0200

    feat: clear proyect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TeamsModule } from './teams/teams.module';
-import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [TeamsModule, PlayersModule],
+  imports: [],
 })
 export class AppModule {}
